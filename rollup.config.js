e8440d71590497b21e055fb21b241f15cced41b3
Jack.Yang <1235388+ylkjick532428@users.noreply.github.com>
2021-07-19 22:41:34 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+import serve from 'rollup-plugin-serve';
+
+const packageJson = require("./package.json");
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss(),
+    serve('dist')
+  ],
+  watch: {
+    chokidar: {
+      // if the chokidar option is given, rollup-watch will
+      // use it instead of fs.watch. You will need to install
+      // chokidar separately.
+      //
+      // this options object is passed to chokidar. if you
+      // don't have any options, just pass `chokidar: true`
+    },
+
+    // include and exclude govern which files to watch. by
+    // default, all dependencies will be watched
+    exclude: ['node_modules/**']
+  }
+};
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import FixedContainer from "./app";
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+ReactDOM.render(
+  <FixedContainer></FixedContainer>,
+  document.getElementById('root')
+);
+
+<AppBar position="fixed" color="primary">
+  <Toolbar>
+    <Typography variant="h6">
+      
+    </Typography>
+  </Toolbar>
+</AppBar>
